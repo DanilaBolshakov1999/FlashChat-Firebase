d1b72f67bb877f5f3c123778d82f04edafa165eb
Danila Bolshakov <danila1999777@gmail.com>
2023-09-16 16:45:13 +0300

    Commit

--- FlashChat-Firebase/Controllers/LoginViewController.swift
+++ FlashChat-Firebase/Controllers/LoginViewController.swift
@@ -15,6 +15,8 @@ final class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    
+    
     @IBAction func loginPressed(_ sender: UIButton) {
         if let email = emailTextField.text, let password = passwordTextField.text {
             Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
